d191ba4ec461c0ae10c8bff801a2a6c021a4125c
tarunchhabra862-creator <tarunchhabra862@gmail.com>
2026-07-27 13:42:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ if st.button("Generate Resume"):
     code = response['messages'][-1].content[-1]['text']
 
    # st.markdown(code)
-   st.html( code, width = "stretch", unsafe_allow_javascript= True)
+   st.html( code, width = "stretch", unsafe_allow_javascript=True)
 
 
 
