ef8ecb645860bef76c4ce288b888e4f2136a7e69
tarunchhabra862-creator <tarunchhabra862@gmail.com>
2026-07-28 13:56:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -149,4 +149,4 @@ if st.button('generate resume'):
     st.divider()
     response = agent.invoke({'messages':[{'role':'user','content': job_prompt}]})
     job_code = response['messages'][-1].content[-1]['text']
-    st.html(code , width="stretch" , unsafe_allow_javascript=True)
+    st.html(job_code , width="stretch" , unsafe_allow_javascript=True)
