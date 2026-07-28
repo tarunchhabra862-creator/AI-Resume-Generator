b7253ef3bbc180f16e2f1b35606949e8e7c4532b
tarunchhabra862-creator <tarunchhabra862@gmail.com>
2026-07-28 14:05:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -134,7 +134,7 @@ if st.button('generate resume'):
   with st.spinner("runnign agent"):
 
     response = agent.invoke({'messages': [{'role':'user','content':query}]})
-    print(response['messages'][-1].content)
+    
     code=response['messages'][-1].content[-1]['text']
 
     # swap in the actual uploaded photo instead of the placeholder tag
@@ -144,7 +144,7 @@ if st.button('generate resume'):
         data_uri = f"data:image/jpeg;base64,{b64_image}"
         code = code.replace("PROFILE_IMAGE_PLACEHOLDER", data_uri)
 
-        st.html(code , width="stretch" , unsafe_allow_javascript=True)
+    st.html(code , width="stretch" , unsafe_allow_javascript=True)
       
    
     st.divider()
